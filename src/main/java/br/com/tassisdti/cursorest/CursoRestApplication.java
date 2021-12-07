544d9683c3aa7d021eecb71d4b99ae8e52d3b4a2
Tássis Fernando <tassis.fernandes@dtidigital.com.br>
2021-12-07 12:46:00 -0300

    first commit

---
+++ src/main/java/br/com/tassisdti/cursorest/CursoRestApplication.java
@@ -0,0 +1,13 @@
+package br.com.tassisdti.cursorest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tassisdti/cursorest/CursoRestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tassisdti.cursorest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
